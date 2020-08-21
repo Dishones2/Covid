782ebe9b9fdaced313bf6fb4bc7282ed667dae90
Dishones2 <andrej13524@gmail.com>
2020-08-21 19:34:36 +0600

    added desktop version

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.prevention__slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 7000,
+        arrows: false,
+        dots: true,
+        fade: true
+    });
+});
